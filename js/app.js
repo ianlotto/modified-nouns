26c33c48d0ce25992caadc73936b288b800808b4
Ian Lotto <ianlotto@gmail.com>
2015-12-18 23:32:07 -0500

    adds Loader.getImages

--- js/app.js
+++ js/app.js
@@ -2,9 +2,17 @@
 
 angular.module('modifiedNouns', [])
 
-.constant('MODIFIED_NOUNS', 'data/modified_nouns.json')
+.constant('ASSET_DATA', {
+  mnSrc: 'data/modified_nouns.json',
+  img: {
+    template: 'data/{0}/{1}.jpg',
+    regExp: /\{(\d)\}/g,
+    levels: 5,
+    tiles: 4
+  }
+})
 
-.factory('Loader', function ($window, $timeout, $http, MODIFIED_NOUNS) {
+.factory('Loader', function ($window, $timeout, $q, $http, ASSET_DATA) {
   
   var hasLocalStorage = (function () {
     try {
@@ -27,7 +35,7 @@ angular.module('modifiedNouns', [])
       var modifiedNouns, promise;
       
       var _http = function () {
-        return $http.get(MODIFIED_NOUNS).then(function (response) {
+        return $http.get(ASSET_DATA.mnSrc).then(function (response) {
           modifiedNouns = response.data;
           
           $window.localStorage.setItem(key, angular.toJson(modifiedNouns));
@@ -44,6 +52,47 @@ angular.module('modifiedNouns', [])
         $timeout(function () { return modifiedNouns; });
       
       return promise;
+    },
+    getImages: function () {
+      var imgData = ASSET_DATA.img;
+      var imgCount = imgData.levels * imgData.tiles;
+      
+      var numLoaded = 0;
+      var tiles = [];
+      var srcData = [];
+      var deferred = $q.defer();
+      
+      var tile;
+      
+      var _getImage = function () {
+        tile = new $window.Image();
+        tiles.push(tile);
+        
+        tile.onload = function () {
+          numLoaded++;
+          
+          if(numLoaded === imgCount) {
+            deferred.resolve(tiles);
+          }
+        };
+        
+        tile.src = imgData.template.replace(imgData.regExp,
+          function (match, number) {
+            return srcData[ $window.parseInt(number) ];
+          }
+        );
+      };
+      
+      for (var i = 0; i < imgData.levels; i++) {
+        srcData[0] = Math.pow(2, i);
+        
+        for (var j = 0; j < imgData.tiles; j++) {
+          srcData[1] = j    
+          _getImage();
+        }
+      }
+      
+      return deferred.promise;
     }
   };
   
@@ -52,5 +101,9 @@ angular.module('modifiedNouns', [])
 .run(function (Loader) {
   Loader.getModifiedNouns().then(function (data) {
     
+  });
+  
+  Loader.getImages().then(function (data) {
+    console.log(data);
   });
 });

3cf15cfa6a1e0b537dd6b5290d5f46744bbf0312
Ian Lotto <ianlotto@gmail.com>
2015-12-23 01:20:43 -0500

    Adds Gruntfile and addtl configs

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function (grunt) {
+  'use strict';
+  
+  grunt.initConfig({
+    jscs: {
+      src: ['Gruntfile.js', 'js/*.js'],
+      options: {
+        config: '.jscsrc',
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      all: ['Gruntfile.js', 'js/*.js']
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
+  
+  grunt.registerTask('test', ['jshint']);
+};

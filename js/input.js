b039bd628f376abd4e91aee06e36faec714b418c
Ian Lotto <ianlotto@gmail.com>
2015-12-28 23:07:30 -0500

    adds dragging and activeTouches props to Input

--- js/input.js
+++ js/input.js
@@ -27,10 +27,55 @@ angular.module('modifiedNouns.input', [])
 })
 
 // TODO: differentiate between single and multi touch events
-.factory('Input', function () {
-  var pos = {};
+.factory('Input', function ($window, $document) {
+  var activeTouches = { length: 0 };
 
-  var touch, changedTouch;
+  var touch, changedTouch, touches;
+
+  //PROBLEM:
+
+  //Differentiate between dragging and zooming with touch
+  //Google maps can handle both at the same time
+
+  //If one touch present, we're just dragging and we're done
+
+  //If more than two touches are present, use the first two in the array and draw a line between them on each touchmove.
+
+  // Measure first touch in array for getting the dragging
+  // Measure its change in length for getting the zooming
+
+  var getTouches = function (e) {
+    touches = e.changedTouches;
+    return (!!touches && touches.length > 0 && touches) || null;
+  };
+
+  var updateActiveTouches = function (e, destroy) {
+    touches = getTouches(e);
+
+    for (var i = 0; i < touches.length; i++) {
+      touch = touches[i];
+
+      if(destroy) {
+        delete activeTouches[touch.identifier];
+      } else {
+        activeTouches[touch.identifier] = { x:  touch.pageX, y: touch.pageY };
+      }
+    }
+
+    activeTouches.length = $window.Object.keys(activeTouches).length - 1;
+  };
+
+  $document.on('touchstart', function (e) {
+    updateActiveTouches(e);
+  });
+
+  $document.on('touchmove', function (e) {
+    updateActiveTouches(e);
+  });
+
+  $document.on('touchend touchcancel', function (e) {
+    updateActiveTouches(e, true);
+  });
 
   return {
 
@@ -40,6 +85,9 @@ angular.module('modifiedNouns.input', [])
       end: 'mouseup touchend'
     },
 
+    dragging: false,
+    activeTouches: activeTouches,
+
     // Normalize between mouse and touch
     getPos: function (e) {
       touch = (!!e.touches && e.touches.length > 0 && e.touches[0]) || e;
@@ -47,10 +95,11 @@ angular.module('modifiedNouns.input', [])
 
       touch = changedTouch || touch;
 
-      pos.x = touch.pageX;
-      pos.y = touch.pageY;
-
-      return pos;
+      return {
+        id: touch.identifier || touch.type,
+        x:  touch.pageX,
+        y:  touch.pageY
+      };
     }
   };
 });

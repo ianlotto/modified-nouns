f3ef10432d35784c6a95bba68f368333dc603d0b
Ian Lotto <ianlotto@gmail.com>
2015-12-29 16:23:13 -0500

    adds production task to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,14 @@ module.exports = function (grunt) {
   'use strict';
 
   grunt.initConfig({
+
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      all: ['Gruntfile.js', 'js/*.js']
+    },
+
     jscs: {
       src: ['Gruntfile.js', 'js/*.js'],
       options: {
@@ -9,16 +17,81 @@ module.exports = function (grunt) {
       }
     },
 
-    jshint: {
-      options: {
-        jshintrc: true
+    concat: {
+      options: { separator: '\n\n' },
+      dist: {
+        src: ['js/app.js', 'js/*.js'],
+        dest: 'js/app.min.js'
       },
-      all: ['Gruntfile.js', 'js/*.js']
+      css: {
+        src: ['css/app.css', 'css/*.css'],
+        dest: 'css/app.min.css'
+      }
+    },
+
+    ngAnnotate: {
+      options: {
+        singleQuotes: true
+      },
+      dist: {
+        files: {
+          'js/app.min.js': 'js/app.min.js'
+        }
+      }
+    },
+
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      dist: {
+        files: {
+          'css/app.min.css': 'css/app.min.css'
+        }
+      }
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          'js/app.min.js': 'js/app.min.js'
+        }
+      }
+    },
+
+    processhtml: {
+      options: {
+      },
+      dist: {
+        files: {
+          'index.html': ['index.html']
+        }
+      }
     }
+
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-jscs');
 
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-ng-annotate');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-processhtml');
+
   grunt.registerTask('test', ['jshint', 'jscs']);
+
+  grunt.registerTask('production', function () {
+    grunt.task.run([
+      'test',
+      'concat',
+      'ngAnnotate',
+      'cssmin',
+      'uglify',
+      'processhtml'
+    ]);
+  });
+
 };

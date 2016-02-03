4a7ea5f4c875523bcf15ee63e19daa5a6862026a
Ian Lotto <ianlotto@gmail.com>
2016-02-02 23:54:25 -0500

    begins Limit.onResize

--- js/limit.js
+++ js/limit.js
@@ -2,12 +2,12 @@
 
 angular.module('modifiedNouns.limit', [])
 
-.factory('Limit', function () {
+.factory('Limit', function (ModifiedNouns) {
   var orderedLimits = [];
   var limits = {};
   var limitCheck = { limits: limits };
 
-  var widerParent, tallerParent;
+  var widerParent, tallerParent, curLevel;
 
   return {
     setXY: function (startData, parentData) {
@@ -38,7 +38,56 @@ angular.module('modifiedNouns.limit', [])
       limitCheck.z = pos.z < limits.minZ ? -1 : pos.z > limits.maxZ ? 1 : 0;
 
       return limitCheck;
+    },
+
+    onResize: function (parentData) {
+      curLevel = ModifiedNouns.getCurLevel();
+
+      var diffX = parentData.width - (curLevel.position.left + curLevel.size.width);
+
+      // TODO: quick resizes don't work.
+
+      if(curLevel.size.width < parentData.width) {
+
+        // - when level is smaller than window and window is being shrunk, first take away from right / bottom, then take away from left / top.
+        if(diffX < 0) {
+
+          ModifiedNouns.positionLevel(
+            curLevel,
+            Math.floor(parentData.width - curLevel.size.width),
+            curLevel.position.top
+          );
+
+        }
+
+      } else {
+        // - when level is larger than window in a dim and window is being enlarged the level should be anchored to enlarged side, if need be.
+
+        if(diffX > 0) {
+
+          ModifiedNouns.positionLevel(
+            curLevel,
+            curLevel.position.left + diffX,
+            curLevel.position.top
+          );
+
+        }
+
+      }
+
     }
+
   };
 
+})
+
+.directive('watchResize', function ($window, Limit) {
+  return {
+    restrict: 'A',
+    link: function (scope, element) {
+      angular.element($window).on('resize', function (e) {
+        Limit.onResize(element[0].getBoundingClientRect());
+      });
+    }
+  };
 });

f9b2d38b34b288e02b11f1784dafb441b9b4fc13
Ian Lotto <ianlotto@gmail.com>
2016-02-20 18:15:22 -0500

    has initial search working

--- js/search.js
+++ js/search.js
@@ -28,16 +28,29 @@ angular.module('modifiedNouns.search', [])
   };
 })
 
-.directive('search', function (ModifiedNouns, Search) {
+.directive('search', function (ModifiedNouns, FlingAnimation, Search) {
   return {
     restrict: 'A',
     link: function (scope) {
-      scope.modifiedNouns = ModifiedNouns;
+      var curLevel;
 
+      scope.modifiedNouns = ModifiedNouns;
       scope.input = null;
 
       scope.search = function () {
         scope.matches = !!scope.input ? Search.search(scope.input) : [];
+      };
+
+      scope.goTo = function (match) {
+        console.log(match);
+        curLevel = ModifiedNouns.getCurLevel();
+
+        FlingAnimation.start({
+          startX: curLevel.position.left,
+          finishX: match.column * -200,
+          startY: curLevel.position.top,
+          finishY: match.row * -200
+        }, curLevel);
       };
 
       scope.$watch('modifiedNouns.data', function (data) {

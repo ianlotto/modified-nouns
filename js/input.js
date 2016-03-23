a18fb943178ed11dab617ab594b1a37d9861ebec
Ian Lotto <ianlotto@gmail.com>
2016-03-23 00:34:38 -0400

    lints files

--- js/input.js
+++ js/input.js
@@ -266,7 +266,7 @@ angular.module('modifiedNouns.input', [])
       };
     };
 
-    //directive.$inject = ['$window', '$parse', 'Input'];
+    // TODO: directive.$inject = ['$window', '$parse', 'Input'];
 
     inputModule.directive(directiveName, directive);
   };
